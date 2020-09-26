2882d29407aa418e4d721705609ab16c8f14444f
robSyntax <48064476+Kanhalelor@users.noreply.github.com>
2020-09-26 13:43:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// ==== SELECT DOM ELEMENTS =========//
+
+const btn = document.getElementById('readMore');
+const searchBtn = document.getElementById('search-box');
+
+// ==== TEMPLATE ELEMENTS ======//
+
+const btnTemplate = document.createElement('button');
+const btnText = document.createTextNode('Load more articles');
+btnTemplate.appendChild(btnText);
+btnTemplate.setAttribute('id', 'readMore');
+btnTemplate.className = 'more-articles';
+
+// ====== FUNCTIONS ======//
+
+const displayNextSection = (e) => {
+  const sec2 = document.querySelector('.section2')
+  sec2.style.display = 'block';
+  sec2.appendChild(btnTemplate);
+
+  //-- remove button ---//
+  e.currentTarget.remove();
+
+}
+
+const searchArticles = (e) => {
+  console.log(e.currentTarget.value)
+}
+
+// ======= EVENTS ============//
+btn.addEventListener('click', displayNextSection)
+searchBtn.addEventListener('keyup', searchArticles)
+
+//===== DEBUGGING SECTION =====//
+
+// console.log(searchBtn);
